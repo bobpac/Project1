62597d1753f801c5c13e4941679e4b3888356c5f
bobpac <141352390+bobpac@users.noreply.github.com>
2023-08-21 10:02:03 -0400

    Initial Code Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*----- constants -----*/
+/*----- state variables -----*/
+/*----- cached elements  -----*/
+const sqrs = document.getElementById('board')
+
+/* event listeners */
+sqrs.addEventListener('click',function(evt) {
+  let sqrId = evt.target.id;
+});
+
+/*----- functions -----*/
