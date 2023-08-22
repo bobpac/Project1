60272e784e290bcb205da55c7d87b66d9d909441
bobpac <141352390+bobpac@users.noreply.github.com>
2023-08-22 09:11:31 -0400

    Save current status

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 /*----- constants -----*/
-const EMOJI = { 
-  "img" : "",                                    // 0
-  "img" : "img/emoji/ambulance.png",             // 1
-  "img" : "img/emoji/art.png",                   // 2
-  "img" : "img/emoji/bell.png",                  // 3
-  "img" : "img/emoji/boom.png",                  // 4
-  "img" : "img/emoji/cake.png",                  // 5
-  "img" : "img/emoji/camel.png",                 // 6
-  "img" : "img/emoji/church.png",                // 7
-  "img" : "img/emoji/dog2.png",                  // 8 
-  "img" : "img/emoji/duck.png",                  // 9 
-  "img" : "img/emoji/eagle.png",                 // 10
-  "img" : "img/emoji/earth_americas.png",        // 11
-  "img" : "img/emoji/elephant.png",              // 12
-  "img" : "img/emoji/eyes.png",                  // 13
-  "img" : "img/emoji/fire_engine.png",           // 14
-  "img" : "img/emoji/floppy_disk.png",           // 15
-};
+"use strict;"
+const EMOJI = [ 
+  { "img" : ""                                 },  // 0
+  { "img" : "img/emoji/ambulance.png",         },  // 1
+  { "img" : "img/emoji/art.png",               },  // 2
+  { "img" : "img/emoji/bell.png",              },  // 3
+  { "img" : "img/emoji/boom.png",              },  // 4
+  { "img" : "img/emoji/cake.png",              },  // 5
+  { "img" : "img/emoji/camel.png",             },  // 6
+  { "img" : "img/emoji/church.png",            },  // 7
+  { "img" : "img/emoji/dog2.png",              },  // 8 
+  { "img" : "img/emoji/duck.png",              },  // 9 
+  { "img" : "img/emoji/eagle.png",             },  // 10
+  { "img" : "img/emoji/earth_americas.png",    },  // 11
+  { "img" : "img/emoji/elephant.png",          },  // 12
+  { "img" : "img/emoji/eyes.png",              },  // 13
+  { "img" : "img/emoji/fire_engine.png",       },  // 14
+  { "img" : "img/emoji/floppy_disk.png",       }   // 15
+];
 
 const REBUS_PUZZLE = { // partial list
   "img" : "",                  "solution" : "",  "phoneticSolution" : "",  // 0
@@ -26,17 +27,26 @@ const REBUS_PUZZLE = { // partial list
 };
 
 const MAX_GUESSES = 30;
-const NUM_SQUARES = 30;
+const NUM_EMOJI_PAIRS = 15;
+const NUM_SQUARES = (NUM_EMOJI_PAIRS * 2)
 const NUM_GUESSES_PER_TURN = 2;
 
+const BOARD_SHOW_NUMBER = 0;
+const BOARD_SHOW_EMOJI  = 1;
+const BOARD_SHOW_REBUS  = 2;
+
+const EMOJI_INIT = 0; // Emoji value not assinged yet
+
 /*----- state variables -----*/
-let board
-let emojiBoard
-let numGuesses
+let board;
+let emojiBoard;
+let numGuesses;
 let squareSelected;
-let rebusNum
+let rebusNum;
+let numEmojiPairsFound;
 
 /*----- cached elements  -----*/
+const sqrEls            = [...document.querySelectorAll('#board > div')]
 const boardEl           = document.getElementById('board');
 const statusBoxEl       = document.getElementById('status');
 const rebusAnswerTxtEl  = document.getElementById('rebusAnswerTxt');
@@ -46,9 +56,26 @@ const numGuessesEl      = document.getElementById('numGuessesTxt');
 
 /* event listeners */
 boardEl.addEventListener('click',function(evt) {
+  if ( numGuesses === -1 ) return; // Game hasn't started yet.
+  if ( numEmojiPairsFound ===  NUM_EMOJI_PAIRS ) return; // Game is over
   let id = evt.target.id;
   console.log(`id=${id}`)
-});
+  if ( squareSelected[0].sqrId === 0 ) {
+    /* First square being chosen */
+    squareSelected[0].sqrId = id;
+    squareSelected[0].emojiId = emojiBoard[id-1];
+    board[squareSelected[0].sqrId-1] = BOARD_SHOW_EMOJI;
+  } else if (squareSelected[1].sqrId === 0 ) {
+    /* Second square being chosen */
+    squareSelected[1].sqrId = id;
+    squareSelected[1].emojiId = emojiBoard[id-1];
+    board[squareSelected[1].sqrId-1] = BOARD_SHOW_EMOJI;
+    numGuesses += 1;
+  } else {
+    /* We don't want the user to pick a third square. */
+  }
+  render();
+} );
 statusBoxEl.addEventListener('click',function(evt) {
   let id = evt.target.id;
   console.log(`id=${id}`)
@@ -61,16 +88,112 @@ rebusAnswerBtnEl.addEventListener('click',function(evt) {
   let id = evt.target.id;
   console.log(`id=${id}`)
 });
+
 playGameBtnEl.addEventListener('click',function(evt) {
   let id = evt.target.id;
   console.log(`id=${id}`)
+  emojiBoard = [ 
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 
+  ];
+  numGuesses = 0;
+  setStatusBoxMsg('');
+  initSquareSelected();
+  render();
 });
 
 /*----- functions -----*/
 function init() {
-   board = Array(MAX_SQUARES).fill(0);
-   emojiBoard = Array(MAX_SQUARES).fill(0);
-   numGuesses = 0;
-   squareSelected = Array(NUM_GUESSES_PER_TURN).fill(0);
-   rebusNum = 1;
+  board = Array(NUM_SQUARES).fill(BOARD_SHOW_NUMBER);
+  emojiBoard = Array(NUM_SQUARES).fill(EMOJI_INIT);
+  /* emojiBoard = [ 
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 
+  ]; */
+  numGuesses = -1;
+  rebusNum = 1;
+  numEmojiPairsFound = 0;
+  appendStatusBoxMsg("To begin, click on 'Play Game'");
+  render();
 }
+
+function setStatusBoxMsg(msg) {
+  statusBoxEl.innerText = msg;
+}
+
+function appendStatusBoxMsg(msg) {
+  statusBoxEl.append(msg);
+}
+
+function initSquareSelected() {
+  squareSelected = [
+    { "sqrId" : 0, "emojiId" : 0 },
+    { "sqrId" : 0, "emojiId" : 0 } 
+  ];
+}
+
+function renderBoard() {
+  for (let x = 0; x < board.length; x++) {
+    let id = x + 1;
+    switch ( board[x] ) {
+       case BOARD_SHOW_NUMBER:
+         break;
+       case BOARD_SHOW_EMOJI:
+         sqrEl = sqrEls[x];
+         /* const img = document.createElement('img');
+         img.
+         sqlEl.appendChild(img); */
+         console.log(sqrEl)
+         break;
+       case BOARD_SHOW_REBUS:
+         break;
+      default:
+        alert("Invalid board value");
+    }
+  }
+}
+
+function renderStatusBox() {
+  if ( numGuesses === -1 ) return;
+
+  if ( squareSelected[0] === 0 ) {
+    statusBoxEl.innerText = "Select first square";
+  }
+}
+
+function renderRebusAnswer() {
+  if ( numEmojiPairsFound ===  NUM_EMOJI_PAIRS ) {
+    rebusAnswerTxtEl.style.visibility = 'visible';
+    rebusAnswerBtnEl.style.visibility = 'visible';
+  } else {
+    rebusAnswerTxtEl.style.visibility = 'hidden';
+    rebusAnswerBtnEl.style.visibility = 'hidden';
+  }
+}
+
+function renderPlayGameBtn() {
+  console.log(`numGuesses = ${numGuesses}`)
+  if ( numGuesses ===  -1  ) {
+    playGameBtnEl.style.visibility = 'visible';
+  } else {
+    playGameBtnEl.style.visibility = 'hidden';
+  }
+}
+
+function renderNumGuesses() {
+  if ( numGuesses === -1 || numEmojiPairsFound ===  NUM_EMOJI_PAIRS ) {
+    numGuessesEl.style.visibility = 'hidden';
+  } else {
+    numGuessesEl.style.visibility = 'visible';
+  }
+  numGuessesEl.innerText = `Number of Guesses = ${numGuesses} out of ${MAX_GUESSES}`
+}
+
+function render() {
+  renderBoard();
+  renderStatusBox();
+  renderRebusAnswer();
+  renderPlayGameBtn();
+  renderNumGuesses();
+}
+
+/* ------------ MAIN ------------  */
+init();
